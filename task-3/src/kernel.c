afbdf868480447a5d1f5be0a5186ad6b646895ca
Ubuntu <azureuser@VMhilmi.hq4azfnhfwze5gupufa0kltw1f.oslx.internal.cloudapp.net>
2024-05-24 15:25:33 +0000

    feat: base code, no bochs

--- task-3/src/kernel.c
+++ task-3/src/kernel.c
@@ -1,6 +1,44 @@
 #include "kernel.h"
 #include "std_lib.h"
 
+void printString(char* str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        interrupt(0x10, 0x0E << 8 | str[i], 0, 0, 0);
+        i++;
+    }
+}
+
+void readString(char* buf) {
+    int i = 0;
+    char input;
+
+    while (1) {
+        input = interrupt(0x16, 0, 0, 0, 0); // Baca karakter dari keyboard
+        if (input == '\r') { // Jika tombol Enter ditekan
+            buf[i] = '\0'; // Menambahkan null-terminator pada akhir string
+            break; // Keluar dari loop
+        } else if (input >= ' ' && input <= '~') { // Karakter ASCII yang valid
+            buf[i] = input; // Menyimpan karakter pada buffer
+            interrupt(0x10, 0x0E << 8 | input, 0, 0, 0); // Menampilkan karakter yang dibaca
+            i++; // Pindah ke indeks berikutnya
+        }
+    }
+}
+
+void clearScreen() {
+    int i;
+
+    // Membersihkan layar dengan mengisi setiap karakter dengan spasi
+    for (i = 0; i < 80 * 25; i++) {
+        putInMemory(0xB800, i * 2, ' '); // Set karakter ke spasi
+        putInMemory(0xB800, i * 2 + 1, 0x07); // Atur warna karakter ke putih
+    }
+
+    // Set kursor ke posisi awal (0, 0)
+    interrupt(0x10, 0x02 << 8, 0, 0, 0);
+}
+
 int main() {
   char buf[128];
   clearScreen();
--- task-3/src/std_lib.c
+++ task-3/src/std_lib.c
@@ -1,1 +1,51 @@
-#include "stdlib.h"
+#include "std_lib.h"
+
+int div(int a, int b) {
+    return a / b;
+}
+
+int mod(int a, int b) {
+    return a % b;
+}
+
+void memcpy(byte* src, byte* dst, unsigned int size) {
+    unsigned int i;
+    for (i = 0; i < size; i++) {
+        dst[i] = src[i];
+    }
+}
+
+unsigned int strlen(char* str) {
+    unsigned int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+bool strcmp(char* str1, char* str2) {
+    unsigned int i = 0;
+    while (str1[i] != '\0' || str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+void strcpy(char* src, char* dst) {
+    unsigned int i = 0;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+}
+
+void clear(byte* buf, unsigned int size) {
+    unsigned int i;
+    for (i = 0; i < size; i++) {
+        buf[i] = 0;
+    }
+}
